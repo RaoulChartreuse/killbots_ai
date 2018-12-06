adec828f0862bcc1ab584c8240c7da2ac02a8b0a
RaoulChartreuse <laurent.morin.morin@gmail.com>
2018-12-06 11:17:54 +0100

    move engine updated to move all bots at the same time

--- killbots.py
+++ killbots.py
@@ -1,10 +1,24 @@
 import numpy #I am using numpy to avoid the ugly array in python
 
+def isUp(action):
+    return action < 3
 
+def isDown(action):
+    return action>=6 and action<9
+
+def isLeft(action):
+    return action%3 == 0 and action<9
+
+def isRight(action):
+    return action%3 == 2 and action<9
+    
 class killbots:
     """PLay a game of killbots
     """
     _max_energy = 12
+    _bot_points = 5
+    _fastbot_points = 10
+    _energy_points = 5
 
     def __init__(self, row = 16, col = 16, graph = True):
         self.hx = row / 2
@@ -22,7 +36,7 @@ class killbots:
         self.score = 0
         self.energy = 0
         self.populate(8)
-        
+        self.isDead = False
         
 
     def empty_rnd_cell(self) :
@@ -34,12 +48,14 @@ class killbots:
         return x,y
 
     
+
+    
     def populate(self, N_bot):
         self.land = numpy.zeros((self.row, self.col),
                                 dtype=numpy.uint8)
         self.land[self.row/2][self.col/2] = 1
-        self.hx = row / 2
-        self.hy = col / 2
+        self.hx = self.row / 2
+        self.hy = self.col / 2
         
         for _ in range(N_bot):
             x,y = self.empty_rnd_cell()
@@ -51,7 +67,7 @@ class killbots:
         """Play a round of game, need an action
         Return 1 if hero is still alive
         Return 0 if hero is dead
-        Return -1 if the move is 
+        Return -1 if the move is illegal
         Action code :
         0 = up left 
         1 = up
@@ -66,12 +82,71 @@ class killbots:
         10 = teleport
         11 = teleport safely
         """
-        pass
 
+        #Bounded move
+        if ((isUp(action)  and self.hx == 0 )
+            or (isDown(action) and self.hx == self.row-1)
+            or (isLeft(action) and action<9 and self.hy == 0 )
+            or (isRight(action) and action<9 and self.hy == self.col-1)
+            ) : return -1
+
+        mx=0
+        if isUp(action) : mx=-1
+        if isDown(action) : mx = 1
+        my=0
+        if isLeft(action) : my = -1
+        if isRight(action) : my = 1
+        if ( self.land[self.hx + mx][self.hy + my] == 0):
+            self.land[self.hx][self.hy] = 0
+            self.hx += mx
+            self.hy += my
+            self.land[self.hx][self.hy] = 1
         
+        
+        self.move_bot()
+        if self.isDead : return 0
+        else :return 1
+  
+
+    
+    def move_bot(self, is_fastbot = False):
+        """
+        """
+        bot = 2
+        if is_fastbot : bot = 3 
+
+        bot_xy = numpy.argwhere(self.land == bot)
+
+        keep_item = numpy.argwhere( numpy.logical_and(self.land != bot,self.land > 0))
+        new_land  = numpy.zeros((self.row, self.col),
+                                dtype=numpy.uint8)
+        for xy in keep_item :
+            new_land[xy[0]][xy[1]] = self.land[xy[0]][xy[1]]
+        
+        print bot_xy
+        for xy in bot_xy:
+            print xy, xy[0], xy[1], "  :",
+            mx = xy[0] + numpy.sign(self.hx-xy[0])
+            my = xy[1] + numpy.sign(self.hy-xy[1])
+            print mx, " , ", my 
+            self.land[xy[0]][xy[1]] = 0            
+            if new_land[mx][my] > 0 :
+                if new_land[mx][my] == 1 :
+                    self.isDead = True
+                new_land[mx][my] = 4
+            else :
+                new_land[mx][my] = bot
+        self.land = numpy.copy(new_land)     
         
         
 
 
 a = killbots()
 print a.land
+while not(a.isDead):
+    print "----------------------------"
+    print "Action :"
+    action = input("Action ?")
+    a.play(action)    
+    print a.land
+
